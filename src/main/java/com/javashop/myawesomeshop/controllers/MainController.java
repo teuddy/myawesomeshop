58adf5d015852bf7e26318e2cdc5fd8031321dde
Owner <macowner@Owners-MacBook-Pro.local>
2018-07-30 15:40:09 -0500

    hey

---
+++ src/main/java/com/javashop/myawesomeshop/controllers/MainController.java
@@ -0,0 +1,35 @@
+package com.javashop.myawesomeshop.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+public class MainController {
+
+
+    @GetMapping(value = "/home")
+    public String hello() {
+        InetAddress ip;
+        try {
+
+            ip = InetAddress.getLocalHost();
+            System.out.println("Current IP address : " + ip.getHostAddress());
+
+        } catch (UnknownHostException e) {
+
+            e.printStackTrace();
+
+        }
+
+        return "hello";
+
+    }
+
+
+
+}
+
+
